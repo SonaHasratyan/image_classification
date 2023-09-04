32ddc62eace529addd02f8b87c7c493bb58a27cf
Sona Hasratyan <sonah@10web.io>
2023-09-05 00:23:24 +0400

    getting familiar with pytorch on the MNIST dataset

---
+++ README.md
@@ -0,0 +1,17 @@
+## Setup
+
+conda create -n pytorch python=3.7
+conda activate pytorch
+
+# Install pytorch
+conda install pytorch torchvision -c pytorch
+or with GPU
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+
+# Verification:
+import torch
+x = torch.rand(5, 3)
+print(x)
+
+torch.cuda.is_available()
+pip install -r requirements.txt
---
+++ preprocessing.py
@@ -0,0 +1,25 @@
+"""
+    Image classification task with pytorch, including dataloader, train, inference and much more...
+"""
+
+
+# dataloader, transformation
+# multilayer NN, activation function
+# loss and optimizer
+# training loop (batch training)
+# model evaluation
+# GPU support
+
+import torch
+import torch.nn as nn
+import torchvision
+import torch.transforms as transforms
+import numpy as np
+import pandas as pd
+# import sklearn
+import matplotlib.pyplot as plt
+
+
+class CNNDataloader():
+    pass
+
